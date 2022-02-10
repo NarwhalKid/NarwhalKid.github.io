caa7e9f8ffb19bd4dc2b7e4b9482754a36e936e8
Narwhal Kid <55326739+NarwhalKid@users.noreply.github.com>
2022-02-10 12:45:33 -0500

    Create py.js

---
+++ games/py.js
@@ -0,0 +1,4 @@
+var front = 'https://nkpydodge';
+var end = '.herokuapp.com/course/https://www.narwhalkid.com/blockedtest/'
+var el = document.getElementById('py');
+el.src = front+num+end; // assign url to src property
