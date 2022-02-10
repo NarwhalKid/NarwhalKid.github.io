a3e9be1811f72187686c80b262a1d99a4ce07984
Narwhal Kid <55326739+NarwhalKid@users.noreply.github.com>
2022-02-10 13:56:06 -0500

    Update alloy.js

--- games/alloy.js
+++ games/alloy.js
@@ -1,4 +1,4 @@
 var front = 'https://nkalloy';
-var end = '.herokuapp.com/web/_aHR0cDovL25hcndoYWxraWQuY29t_/blockedtest'
+var end = '.herokuapp.com/web/_aHR0cHM6Ly93d3cubmFyd2hhbGtpZC5jb20=_/blockedtest/'
 var el = document.getElementById('ifrm');
 el.src = front+num+end; // assign url to src property
