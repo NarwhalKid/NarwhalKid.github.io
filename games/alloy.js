4b93f8e814b6f930601b3d3a23d3e9283a570eb1
Narwhal Kid <55326739+NarwhalKid@users.noreply.github.com>
2022-02-10 12:44:24 -0500

    Create alloy.js

---
+++ games/alloy.js
@@ -0,0 +1,4 @@
+var front = 'https://nkalloy';
+var end = '.herokuapp.com/web/_aHR0cDovL25hcndoYWxraWQuY29t_/blockedtest'
+var el = document.getElementById('ifrm');
+el.src = front+num+end; // assign url to src property
