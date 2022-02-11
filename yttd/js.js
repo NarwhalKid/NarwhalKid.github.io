ac56ba90155b8ca41e8ffd3785705d51bc78e5c3
Narwhal Kid <55326739+NarwhalKid@users.noreply.github.com>
2022-02-10 19:16:09 -0500

    kjh

---
+++ yttd/js.js
@@ -0,0 +1,4 @@
+var front = 'https://nkalloy';
+var end = '.herokuapp.com/web/_aHR0cHM6Ly92Z3BlcnNvbi5jb20=_/games/yourturntodie/'
+var el = document.getElementById('ifrm');
+el.src = front+num+end; // assign url to src property
