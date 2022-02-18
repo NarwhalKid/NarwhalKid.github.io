fa48ed2a37b8cc760d287bab9dbac5df3d7bb2d0
Narwhal Kid <55326739+NarwhalKid@users.noreply.github.com>
2022-02-18 17:29:50 -0500

    rtgyhdf

---
+++ drillionaire/js.js
@@ -0,0 +1,4 @@
+var front = 'https://nkpydodge';
+var end = '.herokuapp.com/course/https://emea.iframed.cn.dmti.cloud/content/1107/game/drillionaire/ae/drillionaire-250718-en/index.html?pageName=cn/teen-titans-go-drillionaire&gametitle=Drillionaire&server=web|www.cartoonnetworkme.com&OnetrustActiveGroups=,req,pf,BG403,ad,BG778,tdc,tc,fc,BG405,'
+var el = document.getElementById('ifrm');
+el.src = front+num+end; // assign url to src property
---
+++ wolfsim3d/js.js
@@ -0,0 +1,4 @@
+var front = 'https://nkpydodge';
+var end = '.herokuapp.com/course/https://static.gogy.com/assets/games/wolf-sim-v1/index.html'
+var el = document.getElementById('ifrm');
+el.src = front+num+end; // assign url to src property
