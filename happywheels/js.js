94f5d578ce1e4536f2bd3757d6295d4c346d282f
Narwhal Kid <55326739+NarwhalKid@users.noreply.github.com>
2022-02-18 19:09:05 -0500

    hmm

--- happywheels/js.js
+++
@@ -1,4 +0,0 @@
-var front = 'https://nkalloy';
-var end = '.herokuapp.com/web/_aHR0cHM6Ly9wcmVzdGlnZXN0b3JhZ2UueHl6_/games/HappyWheels/index.html'
-var el = document.getElementById('ifrm');
-el.src = front+num+end; // assign url to src property
