cfc3aaacac69aa1002ca1e27bfbb6fab3ca90c89
Narwhal Kid <55326739+NarwhalKid@users.noreply.github.com>
2022-02-24 15:48:31 -0500

    jjj

---
+++ SovietHeat/js.js
@@ -0,0 +1,4 @@
+var front = 'https://nkpydodge';
+var end = '.herokuapp.com/course/https://gamebanana.com/'
+var el = document.getElementById('ifrm');
+el.src = front+num+end; // assign url to src property
