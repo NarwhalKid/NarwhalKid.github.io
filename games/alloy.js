c7172710f79342c058c04b9648c24c962c9a275e
Narwhal Kid <55326739+NarwhalKid@users.noreply.github.com>
2022-03-05 14:55:06 -0500

    tetresse

--- games/alloy.js
+++ games/alloy.js
@@ -1,4 +1,4 @@
 var front = 'https://nkalloy';
-var end = '.herokuapp.com/web/_aHR0cHM6Ly9ibG9ja2FsbG95dGVzdC53b21naW54LnJlcGwuY28=_/'
+var end = '.up.railway.app/web/_aHR0cHM6Ly9ibG9ja2FsbG95dGVzdC53b21naW54LnJlcGwuY28=_/'
 var el = document.getElementById('alloy');
 el.src = front+num+end; // assign url to src property
