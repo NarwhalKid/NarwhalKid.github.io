2b73e470a7798920496ff511fca6918a7ea53b89
Narwhal Kid <55326739+NarwhalKid@users.noreply.github.com>
2022-03-30 12:14:08 -0400

    Update js.js

--- history/js.js
+++ history/js.js
@@ -1,4 +1,26 @@
-var a = 'https://'
-var url = prompt("Enter Link", a);
-var el = document.getElementById('ifrm');
-el.src = url; // assign url to src property
+function proxy() {
+	if (document.getElementById('proxyId').innerText == 'Proxy: On') {
+    document.getElementById('subButton').innerHTML = '<button id=submitId onclick="submit(0)">Submit</button>';
+  document.getElementById('proxyId').innerText = 'Proxy: Off';
+  } else {
+    document.getElementById('subButton').innerHTML = '<button id=submitId onclick="submit(1)">Submit</button>';
+  document.getElementById('proxyId').innerText = 'Proxy: On';
+  }
+}
+
+function submit(onStat) {
+  var x = document.getElementById("link").value;
+  document.getElementById("inside").innerHTML = '<iframe allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen id="ifrm" sandbox="allow-forms allow-modals allow-orientation-lock allow-pointer-lock allow-presentation allow-same-origin allow-scripts allow-top-navigation allow-top-navigation-by-user-activation" src="https://google.com/" width="1920" height="1080" style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;"></iframe>;';
+  
+  
+
+	var el = document.getElementById('ifrm');
+  
+	if (onStat == 1) {
+	var x = "https://nkpydodge" + num + ".herokuapp.com/course/" + x
+	}
+
+
+
+	el.src = x; // assign url to src property
+}
