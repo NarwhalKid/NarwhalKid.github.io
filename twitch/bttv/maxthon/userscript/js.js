1a47f81768d129c8a2b092afb796af640e54bfa4
Narwhal Kid <55326739+NarwhalKid@users.noreply.github.com>
2022-05-01 12:23:43 -0400

    r

--- twitch/bttv/maxthon/userscript/js.js
+++ twitch/bttv/maxthon/userscript/js.js
@@ -1,1 +1,11 @@
+function sleep(milliseconds) {
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
+}
+
 window.location.replace("https://nightdev.com/betterttv/other/betterttv.user.js")
+sleep(1000);
+window.close();
