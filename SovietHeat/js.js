379ea201a99e1a0997b130cbea6dff8388923167
Narwhal Kid <55326739+NarwhalKid@users.noreply.github.com>
2024-08-07 07:14:39 -0400

    Delete SovietHeat directory

--- SovietHeat/js.js
+++
@@ -1,4 +0,0 @@
-var front = 'https://nkpydodge';
-var end = '.herokuapp.com/course/https://gamebanana.com/'
-var el = document.getElementById('ifrm');
-el.src = front+num+end; // assign url to src property
