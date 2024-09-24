2c987a1b96e85c44cc242dcb575023572890c2d0
Narwhal Kid <55326739+NarwhalKid@users.noreply.github.com>
2024-09-24 10:58:51 -0400

    Update script.js

--- games/script.js
+++ games/script.js
@@ -48,9 +48,10 @@ function makeNewGameBox(dispName, link, isIfrm) {
 
 
 games = [
-  ["narwhalgpt", "/chat"], 
+  ["narwhalgpt", "/chat/"], 
   ["1v1.lol", "https://f0il.narwhalkid.com/uv/service/hvtrs8%2F-wuw%2Cncruhcliif.aoo%2Feaoeq%2Feaoe-1t1non%2F"], 
-  ["drift hunters", "./game/drifthunters"], 
+  ["doki doki literature ...", "https://backend.narwhalkid.com/ddlc/"], 
+  ["drift hunters", "./game/drifthunters/"], 
   ["2048Verse", "https://2048verse.com/"], 
   ["minecraft", "./game/minecraft"], 
   ["friday night funkin", "./game/fridaynightfunkin"], 
