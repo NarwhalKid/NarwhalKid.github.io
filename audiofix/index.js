c5d2b06212d9cdc30d0c461e37ae9d61b44bc01a
Narwhal Kid <madgamer2525@gmail.com>
2024-11-15 15:48:34 -0500

    Update index.js

--- audiofix/index.js
+++ audiofix/index.js
@@ -3,7 +3,7 @@
 
 //     const patch = () => {
 //         if (metro.findByProps("setCommunicationModeOn").setCommunicationModeOn) {
-//             metro.findByProps("setCommunicationModeOn").setCommunicationModeOnBackup = metro.findByProps("setCommunicationModeOn").setCommunicationModeOn
+//             
             
 //         }
 //     }
@@ -22,7 +22,10 @@
     "use strict";
 
     // Apply the "instead" patch to the "setCommunicationModeOn" function
-    const unpatch = delete metro.findByProps("setCommunicationModeOn").setCommunicationModeOn
+    const unpatch = () => {
+        metro.findByProps("setCommunicationModeOn").setCommunicationModeOnBackup = metro.findByProps("setCommunicationModeOn").setCommunicationModeOn
+        delete metro.findByProps("setCommunicationModeOn").setCommunicationModeOn
+    }
 
     // Attach the unpatch function to be called during onUnload
     context.onUnload = unpatch;
