79d83c346ad3b98c1d960ebb2d55d2e8d2d5ea9b
Narwhal Kid <madgamer2525@gmail.com>
2024-11-15 15:48:05 -0500

    Update index.js

--- audiofix/index.js
+++ audiofix/index.js
@@ -1,19 +1,31 @@
+// (function (context, metro) {
+//     "use strict";
+
+//     const patch = () => {
+//         if (metro.findByProps("setCommunicationModeOn").setCommunicationModeOn) {
+//             metro.findByProps("setCommunicationModeOn").setCommunicationModeOnBackup = metro.findByProps("setCommunicationModeOn").setCommunicationModeOn
+            
+//         }
+//     }
+
+//     const unpatch = () => {
+//         metro.findByProps("setCommunicationModeOn").setCommunicationModeOn = metro.findByProps("setCommunicationModeOn").setCommunicationModeOnBackup
+//     }
+    
+
+//     context.onUnload = unpatch;
+//     context.onLoad = patch;
+
+// })({}, vendetta.metro);
+
 (function (context, metro) {
     "use strict";
 
-    const patch = () => {
-        if (metro.findByProps("setCommunicationModeOn").setCommunicationModeOn) {
-            metro.findByProps("setCommunicationModeOn").setCommunicationModeOnBackup = metro.findByProps("setCommunicationModeOn").setCommunicationModeOn
-            delete metro.findByProps("setCommunicationModeOn").setCommunicationModeOn
-        }
-    }
+    // Apply the "instead" patch to the "setCommunicationModeOn" function
+    const unpatch = delete metro.findByProps("setCommunicationModeOn").setCommunicationModeOn
 
-    const unpatch = () => {
-        metro.findByProps("setCommunicationModeOn").setCommunicationModeOn = metro.findByProps("setCommunicationModeOn").setCommunicationModeOnBackup
-    }
-    
-
+    // Attach the unpatch function to be called during onUnload
     context.onUnload = unpatch;
-    context.onLoad = patch;
 
-})({}, vendetta.metro);
+    return context;
+})({}, vendetta.metro.common);
