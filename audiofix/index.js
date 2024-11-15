68d6fe56a5d063e4e4c1065eb8985b3793c31416
Narwhal Kid <madgamer2525@gmail.com>
2024-11-15 15:09:38 -0500

    Update index.js

--- audiofix/index.js
+++ audiofix/index.js
@@ -1,21 +1,57 @@
-(function (context, common, patcher) {
-    "use strict";
+export default {
+    
+    onLoad() {
+      // Hook into the WebRTC API or any relevant audio-related function
+      const audioModule = vendetta.metro.findByProps("setCommunicationModeOn");
+  
+      if (audioModule && audioModule.setAudioType) {
+        const originalSetAudioType = audioModule.setAudioType;
+  
+        audioModule.setAudioType = function (type, ...args) {
+          // Prevent setting 'in_call' audio type
+          if (type === "in_call") {
+            console.log("Blocked 'in_call' audio type.");
+            return;
+          }
+          return originalSetAudioType.apply(this, [type, ...args]);
+        };
+  
+        console.log("Patched setAudioType!");
+      } else {
+        console.warn("Audio module not found. Plugin might not work as expected.");
+      }
+    },
+  
+    onUnload() {
+      // Restore original behavior
+      const audioModule = vendetta.metro.findByProps("setCommunicationModeOn");
+      if (audioModule && audioModule.setAudioType) {
+        delete audioModule.setAudioType;
+      }
+    }
+  };
 
-    // Accessing the correct AudioManager module from NativeModules
-    const audioManager = common.ReactNative.NativeModules.AudioManager === null
-        ? common.ReactNative.NativeModules.RTNAudioManager
-        : common.ReactNative.NativeModules.AudioManager;
 
-    // Apply the "instead" patch to the "setCommunicationModeOn" function
-    const unpatch = patcher.instead("setCommunicationModeOn", audioManager, () => {
-        // Empty function for the patch
-    });
+// (function (context, common, patcher) {
+//     "use strict";
 
-    // Attach the unpatch function to be called during onUnload
-    context.onUnload = unpatch;
+//     // Accessing the correct AudioManager module from NativeModules
+//     const audioManager = common.ReactNative.NativeModules.AudioManager === null
+//         ? common.ReactNative.NativeModules.RTNAudioManager
+//         : common.ReactNative.NativeModules.AudioManager;
 
-    return context;
-})({}, vendetta.metro.common, vendetta.patcher);
+//     // Apply the "instead" patch to the "setCommunicationModeOn" function
+//     const unpatch = patcher.instead("setCommunicationModeOn", audioManager, () => {
+//         // Empty function for the patch
+//     });
+
+//     // Attach the unpatch function to be called during onUnload
+//     context.onUnload = unpatch;
+
+//     return context;
+// })({}, vendetta.metro.common, vendetta.patcher);
+
+
 
 
 
