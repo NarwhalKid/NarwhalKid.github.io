ddef2c29ffb353b96f01b5cacdd39db8e6fab843
Narwhal Kid <madgamer2525@gmail.com>
2024-12-30 04:59:47 -0500

    hgjk

---
+++ cs2/script.js
@@ -0,0 +1,72 @@
+
+document.getElementById("search").onclick = ()=>{
+    const item = document.getElementById("skin").value;
+    const pattern = document.getElementById("pattern").checked;
+    const quality = document.getElementById("quality").value;
+    const type = document.getElementById("type").value;
+    const sort = document.getElementById("float").checked ? "float" : "price";
+
+    const url = `https://backend.narwhalkid.com/cs2/item?item=${item}&pattern=${pattern}&sort=${sort}&quality=${quality}&type=${type}`
+    fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+        const resultsDiv = document.getElementById("results");
+        let html = "";
+        if (data.pairs.length < 1) {
+            let resultsError = data.item;
+            if (data.resetTime) resultsError += (new Date(data.resetTime)).toLocaleTimeString();
+            resultsDiv.innerHTML = `<h1>${resultsError}</h1>`;
+            return;
+        }
+        data.pairs.forEach((pair)=>{
+            html += `
+            <hr>
+            <h1>Total Price: $${(pair[0].price + pair[1].price).toFixed(2)}</h1>
+            <div class="pairParent">
+                <div class="item1">
+                    <h2>Price: $${pair[0].price.toFixed(2)}</h2>
+                    <h2>Pattern: ${pair[0].pattern}</h2>
+                    <h2>Float: ${pair[0].float}</h2>
+                    <img src="${pair[0].screenshots.front}">
+                    <br>
+                    <img src="${pair[0].screenshots.back}">
+                    <a href="${pair[0].url}" target="_blank"><h2>Click here to open in CSFloat</h2></a>
+                    <a href="${pair[0].inspect}"><h2>Click here to open in game</h2></a>
+                </div>
+                <div class="item2">
+                    <h2>Price: $${pair[1].price.toFixed(2)}</h2>
+                    <h2>Pattern: ${pair[1].pattern}</h2>
+                    <h2>Float: ${pair[1].float}</h2>
+                    <img src="${pair[1].screenshots.front}">
+                    <br>
+                    <img src="${pair[1].screenshots.back}">
+                    <a href="${pair[1].url}" target="_blank"><h2>Click here to open in CSFloat</h2></a>
+                    <a href="${pair[1].inspect}"><h2>Click here to open in game</h2></a>
+                </div>
+            </div>`
+        })
+        resultsDiv.innerHTML = html + `<hr><h1>You've reached the end.</h1>`;
+    })
+}
+
+document.getElementById("skin").onkeyup = ()=>{
+    const autofill = document.getElementById("autofill");
+    const searchQuery = document.getElementById("skin").value;
+    if (searchQuery == "") {
+        autofill.innerHTML = "";
+        return;
+    }
+    fetch(`https://backend.narwhalkid.com/cs2autocomplete?query=${searchQuery}`)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {        
+        let html = "";
+        data.forEach((elem) => {
+            html += `<h3 class="suggestion" onclick="document.getElementById('skin').value = '${elem}'; document.getElementById('autofill').innerHTML = ''">${elem}</h3><br>`
+        });
+        autofill.innerHTML = html;
+    })
+}
