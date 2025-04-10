25841b8cd7679e7381a24c77128f22ccec82401b
Narwhal Kid <55326739+NarwhalKid@users.noreply.github.com>
2025-04-10 12:38:30 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let i = -1
+let i = -1;
 setInterval(()=>{
     i=(i+1)%360;
     var r = document.querySelector(':root');
     var rs = getComputedStyle(r);
-    rs.getPropertyValue('--highlightColor')
+    rs.getPropertyValue('--highlightColor');
     
     r.style.setProperty('--highlight-color', `hsl(${i} 100% 50%`);
 },500)
